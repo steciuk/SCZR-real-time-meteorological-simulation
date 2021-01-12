609b8d48cc0a42cc6c120354491cc66a3c4408df
justleon <maniak656@gmail.com>
2021-01-13 00:45:10 +0100

    updated histogram creator

--- histogram_creator.py
+++ histogram_creator.py
@@ -6,23 +6,25 @@ import matplotlib.pyplot as plt
 data = []
 with open("log_b.txt", 'r') as f:
     for line in f:
-        #data.append(line)
         words = re.split('\W+', line)
         words = words[-2]
         data.append(int(words))
 
 num_bins = 250
 n, bins, patches = plt.hist(data, num_bins, facecolor='blue', alpha=0.5)
+plt.xlabel("czas wykonywania [ms]")
+plt.ylabel("liczebność w danym zakresie (" + str(num_bins) + " przedziałów)")
 plt.show()
 
 data = []
 with open("log_c.txt", 'r') as f:
     for line in f:
-        #data.append(line)
         words = re.split('\W+', line)
         words = words[-2]
         data.append(int(words))
 
 num_bins = 250
 n, bins, patches = plt.hist(data, num_bins, facecolor='blue', alpha=0.5)
+plt.xlabel("czas wykonywania [ms]")
+plt.ylabel("liczebność w danym zakresie (" + str(num_bins) + " przedziałów)")
 plt.show()
